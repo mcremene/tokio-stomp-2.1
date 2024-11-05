86131a2d76c2282ca68eaf3f0c773ffcfa55b27d
marcel <marcel@aiperion.ai>
2024-11-05 15:16:17 +0200

    add tls support - v.4

--- examples/connect_tls.rs
+++ examples/connect_tls.rs
@@ -13,19 +13,13 @@ use tokio_stomp_2_1::*;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-
     let domain = dotenvy::var("DOMAIN").expect("DOMAIN must be set");
     let host = dotenvy::var("HOST").expect("HOST must be set");
     let user = dotenvy::var("USER").expect("USER must be set");
     let pass = dotenvy::var("PASS").expect("PASS must be set");
 
-    let conn = client::connect_tls(
-        domain.as_str(),
-        host.as_str(),
-        user.into(),
-        pass.into(),
-    )
-    .await?;
+    let conn =
+        client::connect_tls(domain.as_str(), host.as_str(), user.into(), pass.into()).await?;
 
     tokio::time::sleep(Duration::from_millis(200)).await;
 
